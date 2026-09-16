a07735f93225be40969e466a32b1216773c6cd62
Morten Slott Hansen <msh@vivino.com>
2026-09-16 20:34:17 +0200

    Add google-services.json and initialize Crashlytics in MainActivity

--- composeApp/src/androidMain/kotlin/com/sabeomnim/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/sabeomnim/app/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 import com.sabeomnim.app.core.storage.AppSettings
+import com.sabeomnim.app.core.logging.CrashlyticsLogger
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppSettings.init(applicationContext)
+        CrashlyticsLogger.log("Sabeomnim MainActivity initialized")
         enableEdgeToEdge()
         setContent {
             App()
